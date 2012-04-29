7b9b99b97cb17899663a962b82fc8ca74ce54292
Andrew Hurle <fracture91@gmail.com>
2012-04-29 17:08:12 -0400

    Fix bug where isUserAllowedToView tested identity, not equality

--- PointsBoard/points/models.py
+++ PointsBoard/points/models.py
@@ -47,7 +47,7 @@ class Board(models.Model):
 		except User.DoesNotExist:
 			return False
 	def isUserAllowedToView(self, user):
-		return user is self.owner or self.isUserParticipating(user)
+		return user == self.owner or self.isUserParticipating(user)
 	def __unicode__(self):
 		return self.owner.username + "/" + self.name + "." + unicode(self.id)
 	class Meta:
