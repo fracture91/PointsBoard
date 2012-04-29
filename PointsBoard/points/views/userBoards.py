3eb07c35c81d9598cb74c6fec74ad3190a0a1bac
Eric Finn <ericfinn.166+git@gmail.com>
2012-04-29 00:14:48 -0400

    Templated boards.html, made view work with it.

--- PointsBoard/points/views/userBoards.py
+++ PointsBoard/points/views/userBoards.py
@@ -1,7 +1,12 @@
-from django.template import Context, loader
+from django.template import RequestContext, loader
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
+from points.models import Board
 
+@login_required
 def userBoards(request):
-	template = loader.get_template('points/user_boards.html')
-	context = Context()
+	template = loader.get_template('points/boards.html')
+	userBoards = request.user.participating_boards.all()
+	ownedBoards = request.user.owned_boards.all()
+	context = RequestContext(request, {"boards":userBoards, "ownedBoards":ownedBoards})
 	return HttpResponse(template.render(context))
